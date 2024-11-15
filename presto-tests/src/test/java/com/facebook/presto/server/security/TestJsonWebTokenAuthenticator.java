f84a9ef1a4774a353db2cbc3955cee9ccb64eb89
sayarimukherjee <SayariMukherjee@ibm.com>
2024-11-15 14:26:48 +0530

    moved this security package inside the server package in the same root package
    Co-authored-by: namya28 <sehgalnamya@gmail.com>

--- presto-tests/src/test/java/com/facebook/presto/server/security/TestJsonWebTokenAuthenticator.java
+++ presto-tests/src/test/java/com/facebook/presto/server/security/TestJsonWebTokenAuthenticator.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.security;
+package com.facebook.presto.server.security;
 
 import com.facebook.airlift.http.server.AuthenticationException;
 import com.facebook.presto.jwt.PrestoJWTAuthenticatorFactory;
